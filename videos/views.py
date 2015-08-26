4621aa046895ab38d26b9df21c20e4f972a6027d
Rohan Nagalkar <rohan.nagalkar@vertisinfotech.com>
2015-08-26 19:45:35 +0530

    added views.py

--- videos/views.py
+++ videos/views.py
@@ -1,70 +1,69 @@
 from django.http.response import HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.views.decorators.vary import vary_on_headers
 from .models import Video, User
-from django.contrib.auth.decorators import login_required
+from .decorators import cookie_required
 
 
-def list_videos(request):
+
+@cookie_required
+def list_videos_view(request):
     """
         List all titles of the videos available
     """
-    if request.COOKIES.has_key('user_name'):
-        video_list = Video.objects.all()
-        context = {'video_list' : video_list}
-        return render(request, 'videos/index.html', context)
-    else:
-        return render(request, 'home.html', {'error': 'Please login to continue'})
 
-def add_video(request):
+    video_list = Video.objects.filter(user_key=User.objects.get(user_name=request.COOKIES['user_name']).id)
+    context = {'video_list' : video_list}
+    return render(request, 'videos/index.html', context)
+
+
+@cookie_required
+def add_video_view(request):
     """
         Add a video
     """
-    if request.COOKIES.has_key('user_name'):
-        return render(request, 'videos/add_video.html')
-    return HttpResponseBadRequest("Page not Found")
+    return render(request, 'videos/add_video.html')
 
 
-def upload_video(request):
+@cookie_required
+def upload_video_view(request):
     """
         Upload video in the database
     """
-    if request.COOKIES.has_key('user_name'):
-        user_obj = User.objects.get(user_name=request.COOKIES.get('user_name'))
-        file_details = request.FILES['file_name']
+    user_obj = User.objects.get(user_name=request.COOKIES.get('user_name'))
+    file_details = request.FILES['file_name']
 
-        if file_details.content_type.startswith('video/'):
-            Video.objects.create(user_key=user_obj, name=file_details.name, size=file_details.size,
-                                 published=timezone.now(), file=file_details)
-            return redirect(list_videos)
-        else:
-            return HttpResponseBadRequest("Invalid video Format.")
-    return HttpResponseBadRequest("Page not Found")
+    if file_details.content_type.startswith('video/'):
+        Video.objects.create(user_key=user_obj, name=file_details.name, size=file_details.size,
+                             published = timezone.now(),
+                              file=file_details)
 
+        return redirect(list_videos_view)
+    else:
+        return HttpResponseBadRequest("Invalid video Format.")
 
-def details(request, video_id):
+@cookie_required
+def details_view(request, video_id):
     """
         List all the details of the video
     """
-    if request.COOKIES.has_key('user_name'):
-        id = User.objects.get(user_name=request.COOKIES.get('user_name')).id
-        # vd = Video.objects.filter(user_key=User.objects.filter(id=id))
-        video_details=Video.objects.get(id= video_id)
-        context = {'video': video_details}
-        return render(request, 'videos/detail.html', context)
-    return HttpResponseBadRequest("Page not Found")
+    id = User.objects.get(user_name=request.COOKIES.get('user_name')).id
+    # vd = Video.objects.filter(user_key=User.objects.filter(id=id))
+    video_details=Video.objects.get(id= video_id)
+    context = {'video': video_details}
+    return render(request, 'videos/detail.html', context)
 
-
-def delete_video(request, video_id):
+@cookie_required
+def delete_video_view(request, video_id):
     """
         Delete the record from the database
     """
-    if request.COOKIES.has_key('user_name'):
-        video_details=Video.objects.get(pk=video_id)
-        video_details.file.delete()
-        video_details.delete()
-        return redirect(list_videos)
-    return HttpResponseBadRequest("Page not Found")
+    import ipdb;ipdb.set_trace()
+    video_details=Video.objects.get(pk=video_id)
+    video_details.file.delete()
+    video_details.delete()
+    return redirect(list_videos_view)
 
 
 
